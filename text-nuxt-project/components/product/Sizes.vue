1b6fe22100cdfbe4679dd40e9f9e88c07927daa9
Morium IT <majad.shohagh@gmail.com>
2024-05-24 19:05:00 +0600

    product details process 04

---
+++ text-nuxt-project/components/product/Sizes.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+    color: {
+        type: String,
+        required: true,
+    },
+    sizes: {
+        type: Object,
+        required: true,
+    },
+});
+
+console.log(props)
+
+</script>
+
+<template>
+    <div class="w-full">
+        <table class="table-auto w-full">
+            <thead>
+                <tr class="border bg-gray-200">
+                    <th :colspan="(Object.keys(sizes).length+1)" class="border text-left px-2 text-sm">Color: {{ color  }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border">
+                    <th class="border p-1 px-2 text-left w-48 text-sm">Size</th>
+
+                    <td class="border p-1 px-2 text-center text-xs" v-for="(size, indexKey) in sizes" :key="indexKey">{{ size.size }}</td>
+                </tr>
+                <tr class="border">
+                    <td class="border p-1 px-2 text-sm">Stock</td>
+                    <td class="border p-1 px-2 text-center text-xs" v-for="(size, indexKey) in sizes" :key="indexKey">{{ size.stock }}</td>
+                </tr>
+                <tr class="border">
+                    <td class="border p-1 px-2 text-sm">Discount Price</td>
+                    <td class="border p-1 px-2 text-center text-xs" v-for="(size, indexKey) in sizes" :key="indexKey">{{ size.current_price }}</td>
+                    
+                </tr>
+                <tr class="border">
+                    <td class="border p-1 px-2 text-sm">Regular Price</td>
+                    <td class="border p-1 px-2 text-center text-xs" v-for="(size, indexKey) in sizes" :key="indexKey">{{ size.regular_price }}</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+</template>
--- text-nuxt-project/pages/product/details.vue
+++ text-nuxt-project/pages/product/details.vue
@@ -2,19 +2,139 @@
     import { ref } from "vue";
     import Card from 'primevue/card';
 
-    const additionals =  ({
+    import TabView from 'primevue/tabview';
+    import TabPanel from 'primevue/tabpanel';
 
-        
+    const customTabStyle = ref(
+        {
+            headerTitle: {
+                class: 'text-red-500'
+            },
+
+            headerAction:{
+                class: "text-black"
+            }
+
+        }
+    )
+
+    const additionals =  ({
 
                 "white" : {
                     "color_icon": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
                     "color_thumbnails": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
                     "color_galleries": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg,https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg",
+                    "size": {
+                        "s": {
+                            
+                            "color": "black",
+                            "size": "s",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 24
+                        },
+                        
+                        "m": {
+                            "product_id": 5,
+                            "color": "black",
+                            "size": "m",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "l": {
+                            
+                            "color": "black",
+                            "size": "l",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "xl": {
+                            
+                            "color": "black",
+                            "size": "xl",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "xxl": {
+                            
+                            "color": "black",
+                            "size": "xxl",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        }
+                    }
                 },
                 "Black" : {
                     "color_icon": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
                     "color_thumbnails": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
                     "color_galleries": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg,https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg",
+                    "size": {
+                        "s": {
+                            
+                            "color": "black",
+                            "size": "s",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 24
+                        },
+                        
+                        "m": {
+                            "product_id": 5,
+                            "color": "black",
+                            "size": "m",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "l": {
+                            
+                            "color": "black",
+                            "size": "l",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "xl": {
+                            
+                            "color": "black",
+                            "size": "xl",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        },
+                        "xxl": {
+                            
+                            "color": "black",
+                            "size": "xxl",
+                            "regular_price": 200,
+                            "current_price": 150,
+                            "discount": 25,
+                            "dicount_type": 0,
+                            "stock": 0
+                        }
+                    }
                 },
 
             }
@@ -55,29 +175,61 @@
                         <div v-for="(additional, key, indexKey) in additionals" :key="indexKey">
                             <ProductGallery :images="additional" :color="key"/>
                         </div>
-                        
                     </div>
-                    
-                    
-                            <!-- <div class="card flex justify-content-center">
-                                    <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="250" preview />
-                                </div>
-                            <Card>
-                                
-                                <template #title>Simple Card</template>
-                                <template #content>
-                                    <p class="m-0">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-                                        quas!
-                                    </p>
-                                </template>
-                            </Card> -->
-                        </div>
-                        
-                    </div> <!-- Header Div  -->
 
-                    
+                    <div class="w-full mt-3">
+                        <div class="w-full" v-for="(color, key, indexKey) in additionals" :key="indexKey">
+                            <ProductSizes :sizes="color.size" :color="key" />
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-4 gap-2 mt-3 ">
+                        <div class="w-full border rounded-md p-2 bg-white">
+                            <div class="w-full text-sm"><b>Vendor:</b> Cusotm Vendor Name</div>
+                            <div class="w-full text-sm"><b>Total Sale:</b> 11</div>
+                            <div class="w-full text-sm"><b>Total Return:</b> 1</div>
+                            <div class="w-full text-sm"><b>Ratting:</b> 4.1 <Icon name="noto:star"/></div>
+                        </div>
+                        <div class="w-full border rounded-md p-2">
+                            <div class="w-full text-sm"><b>Categories:</b> Category1, Category2, Category3</div>
+                        </div>
+                        <div class="w-full border rounded-md p-2">
+                            <div class="w-full text-sm"><b>Brand:</b> Brand Name</div>
+                        </div>
+                        <div class="w-full border rounded-md p-2">
+                            <div class="w-full text-sm"><b>Countries:</b> Bangladesh</div>
+                            <div class="w-full text-sm"><b>District:</b> Dhaka</div>
+                            <div class="w-full text-sm"><b>Thana/Police:</b> Mirpur</div>
+                        </div>
+                    </div>
+
+                    <!-- Tab start -->
+                    <div class="w-full mt-4">
+                        <div class="card">
+                            <TabView >
+                                <TabPanel header="Details" :pt="customTabStyle">
+                                    <p class="m-0">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                </TabPanel>
+                                <TabPanel header="Feedback" :pt="customTabStyle">
+                                    <p class="m-0">
+                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                                        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                                    </p>
+                                </TabPanel>
+                                
+                            </TabView>
+                        </div>
+                    </div>
+
+                </div>
+
+
                 
+
             </div>
+        </div>
     </NuxtLayout>
 </template>
