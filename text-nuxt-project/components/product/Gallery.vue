09dd146fd6e3887db3d7c44dbcf29d642fc41945
Morium IT <majad.shohagh@gmail.com>
2024-05-24 10:49:31 +0600

    Product Processing 02

---
+++ text-nuxt-project/components/product/Gallery.vue
@@ -0,0 +1,62 @@
+<script setup>
+    import Image from 'primevue/image';
+    import Fieldset from 'primevue/fieldset';
+
+    const props = defineProps({
+        color: {
+            type: String,
+            required: true,
+        },
+        images: {
+            type: Object,
+            required: true,
+        },
+    });
+const { colors } = props;
+
+    const fieldestStyle = (
+        { 
+            root: { 
+                class: 'border px-2'
+            },
+            legend:{
+                class: 'p-0 m-0'
+            }
+        }
+    )
+
+    const imageStyle = (
+        {
+            icon: {
+                class: "text-white"
+            },
+            toolbar: {
+                class: "text-white"
+            }
+
+        }
+    )
+
+
+    console.table(props);
+
+</script>
+
+<template>
+
+    <Fieldset legend="Extra Props" :pt="fieldestStyle">
+        <template #legend>
+            <div class="flex align-items-center pl-2 border-t">
+                <span class="font-bold">Color: {{ color }}</span>
+            </div>
+        </template>
+        <div class="w-full grid grid-cols-3 gap-2">
+            <div class="card justify-content-center" v-for="(gallery, key) in images.color_galleries.split(',')" :key="key">
+                <Image :pt="imageStyle" :src="gallery" alt="Image" preview />
+            </div>
+            
+            
+        </div>
+    </Fieldset>
+
+</template>
--- text-nuxt-project/pages/product/details.vue
+++ text-nuxt-project/pages/product/details.vue
@@ -1,8 +1,25 @@
 <script setup>
     import { ref } from "vue";
-
     import Card from 'primevue/card';
-    import Image from 'primevue/image';
+
+    const additionals =  ({
+
+        
+
+                "white" : {
+                    "color_icon": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
+                    "color_thumbnails": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
+                    "color_galleries": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg,https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg",
+                },
+                "Black" : {
+                    "color_icon": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
+                    "color_thumbnails": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg",
+                    "color_galleries": "https://primefaces.org/cdn/primevue/images/galleria/galleria11.jpg,https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg",
+                },
+
+            }
+        
+    )
 
     onMounted( async () => {
         
@@ -12,30 +29,37 @@
     definePageMeta({
         layout: "dashboard",
     })
+
    
 
     
 </script>
 <template>
     <NuxtLayout :name="layout">
-            <div class="w-full px-3 mt-1">
+        <div class="w-full px-3 mt-1">
+            <div class="shadow-md bg-white w-full h-[calc(100vh-6rem)] overflow-hidden rounded-md">
+                <div class="flex w-full justify-between bg-gray-400 text-white">
+                    <div class="font-semibold mt-1 ml-3">Product Details</div>
+                    <div class="font-semibold ml-1 flex">
+                        <button @click="$router.back()" class="bg-[#800] hover:bg-red-500 text-gray-100 hover:text-black px-4 py-2 text-sm transition delay-100">
+                            <Icon name="gg:arrow-left-o"></Icon>
+                            Back
+                        </button>
+                    </div>                    
+                </div>
 
-                    <div class="shadow-md bg-white w-full h-[calc(100vh-6rem)] overflow-hidden rounded-md">
-                        <div class="flex w-full justify-between bg-gray-400 text-white">
-                            
-                            <div class="font-semibold mt-1 ml-3">Product Details</div>
-                            <div class="font-semibold ml-1 flex">
-                                <button @click="$router.back()" class="bg-[#800] hover:bg-red-500 text-gray-100 hover:text-black px-4 py-2 text-sm transition delay-100">
-                                    <Icon name="gg:arrow-left-o"></Icon>
-                                    Back
-                                </button>
-                            </div>
-                            
+
+                <div class="h-full overflow-y-auto p-4 w-full max-h-[calc(100vh-8.2rem)] flex flex-col bg-gray-100">
+                    
+                    <div class="grid grid-cols-4 gap-2">
+                        <div v-for="(additional, key, indexKey) in additionals" :key="indexKey">
+                            <ProductGallery :images="additional" :color="key"/>
                         </div>
-
-                        <!-- Table list goes here -->
-                        <div class="h-full overflow-y-auto w-full max-h-[calc(100vh-8.2rem)] flex flex-col bg-gray-100">
-                            <div class="card flex justify-content-center">
+                        
+                    </div>
+                    
+                    
+                            <!-- <div class="card flex justify-content-center">
                                     <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="250" preview />
                                 </div>
                             <Card>
@@ -47,7 +71,7 @@
                                         quas!
                                     </p>
                                 </template>
-                            </Card>
+                            </Card> -->
                         </div>
                         
                     </div> <!-- Header Div  -->
